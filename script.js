0e35486ef18f05b1cfd0253df204793f8fe3dda4
wiryawaneo <35023905+wiryawaneo@users.noreply.github.com>
2023-06-26 23:19:35 +0800

    add eventlistener to change grid on mouse enter

--- script.js
+++ script.js
@@ -8,20 +8,22 @@ for (j = 0; j < columnLength; j++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
     container.appendChild(grid);
-    grid.addEventListener("mouseover", colorChange);
+    grid.addEventListener("mouseenter", colorChange);
+    // grid.addEventListener("mouseleave", removeChange);
   }
 }
 
 //give each grid div an id
 let id = 1;
 const box = document.querySelectorAll(".grid").forEach((e) => {
-  //   console.log(e);
   e.id = i - 16;
   i++;
 });
 
 function colorChange(e) {
-  //give each grid div an id
-  //   const box = document.querySelector(".grid");
-  console.log(e.target);
+  e.target.classList.add("active");
+}
+
+function removeChange(e) {
+  e.target.classList.remove("active");
 }
